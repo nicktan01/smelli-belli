2d3382530152f6001d73a6e4159b399271d71a63
Nicholas Tan <nicholas.tan01@gmail.com>
2022-06-01 19:19:32 -0400

    added paths to urls.py in inventory

--- inventory/api/inventory_rest/urls.py
+++ inventory/api/inventory_rest/urls.py
@@ -8,3 +8,36 @@ from .views import (
     api_sizes,
     api_size
 )
+
+urlpatterns = [
+    path(
+        "products/",
+        api_products,
+        name="api_products",
+    ),
+    path(
+        "products/<str:sku>/",
+        api_product,
+        name="api_product",
+    ),
+    path(
+        "scents/",
+        api_scents,
+        name="api_scents",
+    ),
+    path(
+        "scents/<int:pk>",
+        api_scent,
+        name="api_scent",
+    ),
+    path(
+        "sizes/",
+        api_sizes,
+        name="api_sizes",
+    ),
+    path(
+        "size/<int:pk>",
+        api_size,
+        name="api_size",
+    ),
+]
