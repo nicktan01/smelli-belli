43f7b85501cba6b4a08e21192efea26969d43b49
Nicholas Tan <nicholas.tan01@gmail.com>
2022-06-01 15:25:52 -0400

    added inventory models

--- inventory/api/inventory_rest/models.py
+++ inventory/api/inventory_rest/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    sku = models.CharField()
+    price = models.PositiveSmallIntegerField()
+    rating = models.PositiveSmallIntegerField()
+    size = models.PositiveSmallIntegerField()
+    quantity = models.PositiveSmallIntegerField()
+    tags = models.CharField(max_length=50)
+    ingredients = models.CharField(max_length=100)
+    limited_item = models.BooleanField()
+    created = models.DateTimeField()
+    image = models.URLField()
+    description = models.CharField(max_length=200)
+    usage = models.CharField(max_length=50)
+    storage = models.CharField(max_length=50)
+    
+class Scent(models.Model):
+    FRESH = 'Fresh'
+    AMBER = 'Amber'
+    FLORAL = 'Floral'
+    WOODY = 'Woody'
+    FRUITY = 'Fruity'
+    GOURMAND = 'Gourmand'
+    SCENT_CHOICES = [
+        (FRESH, 'Fresh'),
+        (AMBER, 'Amber'),
+        (FLORAL , 'Floral'),
+        (WOODY , 'Woody'),
+        (FRUITY , 'Fruity'),
+        (GOURMAND, 'Gourmand')
+    ]
+    scents = models.CharField(
+        choices=SCENT_CHOICES,
+        default=FRESH,
+    )
+    product = models.ForeignKey(
+        Product,
+        related_name="scents",
+        on_delete=models.CASCADE
+    )
