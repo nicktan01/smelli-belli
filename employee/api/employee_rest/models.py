93779f9d33984bd64366fd8bbae9aef200f8015a
Jaiden <jaidensy07@gmail.com>
2022-06-01 16:50:28 -0700

    created base models for employee microservice

--- employee/api/employee_rest/models.py
+++ employee/api/employee_rest/models.py
@@ -1,3 +1,26 @@
+from tkinter import CASCADE
 from django.db import models
 
-# Create your models here.
+
+class ProductVO(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    sku = models.CharField()
+    price = models.PositiveSmallIntegerField()
+    rating = models.PositiveSmallIntegerField()
+    size = models.PositiveSmallIntegerField()
+    quantity = models.PositiveSmallIntegerField()
+    tags = models.CharField(max_length=50)
+    ingredients = models.CharField(max_length=100)
+    limited_item = models.BooleanField()
+    created = models.DateTimeField()
+    image = models.URLField()
+    description = models.CharField(max_length=200)
+    usage = models.CharField(max_length=50)
+    storage = models.CharField(max_length=50)
+
+class Order(models.Model):
+    products = models.ForeignKey(
+        ProductVO,
+        related_name="order",
+        on_delete=CASCADE
+    )
