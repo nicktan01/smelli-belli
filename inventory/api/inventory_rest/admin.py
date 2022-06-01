8199171de81649238051abc58a4d364b1a7dfd06
Elliott Klaassen <elliottklaassen@gmail.com>
2022-06-01 13:34:19 -0700

    added product and scents to admin

--- inventory/api/inventory_rest/admin.py
+++ inventory/api/inventory_rest/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product, Scent
 
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Scent)
+class ScentAdmin(admin.ModelAdmin):
+    pass
