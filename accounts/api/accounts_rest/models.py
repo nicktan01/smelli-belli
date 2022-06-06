dc20b14c17ca6401a73bcaa2ef352552224d747a
Jaiden <jaidensy07@gmail.com>
2022-06-06 16:33:48 -0700

    small comments for models in accounts services

--- accounts/api/accounts_rest/models.py
+++ accounts/api/accounts_rest/models.py
@@ -8,3 +8,5 @@ class User(AbstractUser):
 
     def get_api_url(self):
         return reverse("api_account_detail", kwargs={"email": self.email})
+            # This sets the User's href to the email address, as set up
+                    # in urls.py
