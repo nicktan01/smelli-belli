c0e282bdc3d26f1faae8b5d9f1c89f1af7c5532c
Cindy <cindymuffin@gmail.com>
2022-06-06 17:00:48 -0700

    map API working kind of

---
+++ ghi/app/src/About.js
@@ -0,0 +1,20 @@
+function About() {
+  return (
+    <iframe
+      width="600"
+      height="450"
+      //   style="border:0"
+      loading="lazy"
+      //   allowfullscreen
+      //   referrerpolicy="no-referrer-when-downgrade"
+      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCXyM88QxyPFMonzZyo46iRtxwLGVXUGZQ
+            &q=United+States"
+    ></iframe>
+  );
+}
+
+export default About;
+
+// key: AIzaSyCXyM88QxyPFMonzZyo46iRtxwLGVXUGZQ
+// signature: 7PHOwQLF1VOYRU6YG27Bs4V4mOI=
+// new signature: 51eok5djhJADeB0wo3wLLqqMAwU=
