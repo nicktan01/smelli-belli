2075e8e5c8f6e25dfa2710011a338dc1e6b2a4df
Cindy <cindymuffin@gmail.com>
2022-06-09 12:09:02 -0700

    fixed broken signup

--- ghi/app/src/SignUp.js
+++ ghi/app/src/SignUp.js
@@ -2,7 +2,7 @@ import { useToken } from "./authApi";
 import { useState, useEffect } from "react";
 
 function SignUp() {
-  const [token, logout, signup] = useToken();
+  const [token, login, logout, signup] = useToken();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
