0829b853db11c84f0b850e1440b741c3fc466cb1
Nicholas Tan <nicholas.tan01@gmail.com>
2022-06-16 17:27:31 -0400

    checking main

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -9,39 +9,17 @@ import ProductList from "./product_pages/ProductList";
 import ProductDetails from "./product_pages/ProductDetail";
 import Login from "./Login";
 import SignUp from "./SignUp";
-import Cart from "./Cart/Cart";
-import Checkout from "./Cart/Checkout";
 import EditAccount from "./EditAccount";
 import BodyQuiz from "./quizzes/BodyQuiz";
 import HomeQuiz from "./quizzes/HomeQuiz";
+import Cart from "./Cart/Cart";
+import Checkout from "./Cart/Checkout";
 import { AuthProvider } from "./authApi";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/products">
-            <Route exact path="/products/:sku" element={<ProductDetails />} />
-            <Route
-              path="/products/body"
-              element={<ProductList category="Body" />}
-            />
-            <Route
-              path="/products/home"
-              element={<ProductList category="Home" />}
-            />
-            <Route path="/products/all" element={<ProductList />} />
-          </Route>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
-        </Routes>
-        <Footer />
         <AuthProvider>
           <Nav />
           <Routes>
@@ -65,6 +43,8 @@ function App() {
             <Route path="/account/edit" element={<EditAccount />} />
             <Route path="/bodyquiz" element={<BodyQuiz />} />
             <Route path="/homequiz" element={<HomeQuiz />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
           </Routes>
           <Footer />
         </AuthProvider>
@@ -74,3 +54,4 @@ function App() {
 }
 
 export default App;
+
