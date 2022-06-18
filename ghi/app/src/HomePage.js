3d25c0b6ba8ed0196e98a6b83fdac34aeb838d07
Jaiden <jaidensy07@gmail.com>
2022-06-17 18:07:54 -0700

    updated grid for main page

--- ghi/app/src/HomePage.js
+++ ghi/app/src/HomePage.js
@@ -61,67 +61,49 @@ class HomePage extends React.Component {
           }}
         />
       </div>
-
-      <div className="Quiz d-flex justify-content-center position-relative">
-        <img className="hero-img-blur flipped"
-          src="/images/hero-img.jpg"
-          height={300}
-          width={null}
-          style={{ objectFit: "cover", width: "100%" }}
-        />
-        <div
-          style={{
-            position: "absolute",
-            top: "50%",
-            transform: "translateY(-50%)",
-            width: "100%",
-            height: "200px",
-            background: "#fff",
-            opacity: "0.5",
-          }}
-        />
-        <div height={500}
-          width={null} style={{
-            position: "absolute",
-            top: "50%",
-            transform: "translateY(-50%)",
-          }}>
-            <h3 style={{
-            position: "absolute",
-            transform: "translateY(-100%)"}}>Find Products Suited For You!</h3>       
-
-            <div width={1000} className="row">
-              <div width={1000} className="col text-center px-5">
-              <NavLink to="/bodyquiz" role="button" className="col text-center btn btn-outline-dark btn-lg">
-                Body Quiz Scent
-              </NavLink >
-              </div>
-              <div width={1000} className="col text-center px-5">
-                <NavLink to="/homequiz" role="button" className="col text-center btn btn-outline-dark btn-lg">
-                    Home Quiz Scent
-                </NavLink>
-              </div>
-            </div>
-
-
-
-        </div>
+        <h3 className="text-center" style={{
+        transform: "translateY(-90%)"}}>Find Products Suited For You!</h3>       
+     
+     <div className="px-5">
+     <div className="quiz-container px-5">
+      <div className="div1 text-center left-img">
+        <img src="/images/body-quiz.jpg"
+        height={300}
+        width={null}
+        style={{ objectFit: "cover", width: "100%" }}
+        ></img>
+        <NavLink to="/bodyquiz" role="button" className="col text-center btn btn-outline-dark btn-lg">
+          Body Quiz Scent
+        </NavLink >
       </div>
+      <div className="div2 text-center right-img">
+        <img src="/images/home-quiz.jpg"
+        height={300}
+        width={null}
+        style={{ objectFit: "cover", width: "100%",}}
+        ></img>
+        <NavLink to="/homequiz" role="button" className="col text-center btn btn-outline-dark btn-lg">
+          Home Quiz Scent
+        </NavLink> 
+      </div>
+    </div>
       
-      <div className="carousel container x-5 y-5">
+        
+    <div className="carousel-container carousel-dark">
+    <div className="carousel container x-5 y-5 div3">
         <div id="carouselExampleCaptions" className="carousel slide mt-5" data-bs-ride="false">
         <div className="carousel-indicators">
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
         </div>
-        <div className="carousel-inner">
+        <div className="carousel-inner carousel-fade ">
           <div className="carousel-item active">
           {this.state.products.map(product => {
               if (product.name === top_candle.name) {
                 return (
                   <div>
                     <NavLink to={"/products/"+ product.sku}>
-                      <img height={630} key={product.sku} src={product.image} className="d-block height-80 mx-auto" alt="headphones.jpeg"/>
+                      <img style={{ objectFit: "cover", width: "100%" }} height={630} key={product.sku} src={product.image} className="d-block height-80 mx-auto" alt="headphones.jpeg"/>
                     </NavLink>
                     <div className="carousel-caption d-none d-md-block stroke-text">
                       <h5>Our top Home Product!</h5>
@@ -138,7 +120,7 @@ class HomePage extends React.Component {
                 return (
                   <div>
                     <NavLink to={"/products/"+ product.sku}>
-                      <img height={630} key={product.sku} src={product.image} className="d-block height-80 mx-auto" alt="headphones.jpeg"/>
+                      <img style={{ objectFit: "cover", width: "100%" }} height={630} key={product.sku} src={product.image} className="d-block height-80 mx-auto" alt="headphones.jpeg"/>
                     </NavLink>
                     <div className="carousel-caption d-none d-md-block stroke-text">
                       <h5>Our top Body Product</h5>
@@ -160,6 +142,11 @@ class HomePage extends React.Component {
         </button>
         </div>
       </div>
+      
+     </div>
+
+    </div>
+
       </React.Fragment>
     );
   }
